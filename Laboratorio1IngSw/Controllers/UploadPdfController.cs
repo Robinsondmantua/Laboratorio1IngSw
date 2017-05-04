c897bb671439af34b62d3032b5e724b1c3ef3d81
Robinsondmantua <rdemantua@hotmail.es>
2017-05-04 22:25:45 +0200

    Pdf Controlador, desplegables.

---
+++ Laboratorio1IngSw/Controllers/UploadPdfController.cs
@@ -0,0 +1,54 @@
+﻿using Laboratorio1IngSw.Models.DB;
+using Laboratorio1IngSw.Models.EntityManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio1IngSw.Controllers
+{
+    public class UploadPdfController : Controller
+    {
+        // GET: UploadPdf
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UploadPdf()
+        {
+            TestPlataformaEntities modelo = new TestPlataformaEntities();
+
+            var consulta = modelo.Grado.Select(c => new SelectListItem
+            {
+                Value = c.IDGrado.ToString(),
+                Text = c.Descripcion,
+                Selected = c.IDGrado.Equals(1)  
+            });
+
+            var grados = new SelectPdfViewModel
+            {
+                ListaGrados = consulta.AsEnumerable()
+            };
+
+            return PartialView(grados);
+        }
+
+        [HttpPost]
+        public ActionResult UploadTemario(HttpPostedFileBase file)
+        {
+            try
+            {
+                if(file.ContentLength > 0)
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return View();
+        }
+    }
+}
---
+++ Laboratorio1IngSw/Models/EntityManager/SelectPdfViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio1IngSw.Models.EntityManager
+{
+    public class SelectPdfViewModel
+    {
+        public int IDGrado { get; set; }
+        public int IDAsignatura { get; set; }
+        public int IDTema { get; set; }
+        public IEnumerable<SelectListItem> ListaGrados { get; set; }
+    }
+}
